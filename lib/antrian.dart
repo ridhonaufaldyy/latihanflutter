f1ae25d537bbf22a5f0ed5143e743ace5833162b
NurainunLubis <Nurainunlubis@upi.edu>
2024-05-23 11:09:53 +0700

    update antrian

--- lib/antrian.dart
+++ lib/antrian.dart
@@ -33,9 +33,6 @@ class _QueuePageState extends State<QueuePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Antrian'),
-      ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -47,81 +44,122 @@ class _QueuePageState extends State<QueuePage> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  width: 300,
-                  height: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
-                      ),
-                    ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.black),
                     onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => antrianUmum()),
-                      );
+                      Navigator.pop(context); // Kembali ke halaman sebelumnya
                     },
-                    child: Text('Antrian Poli Umum'),
                   ),
                 ),
-                SizedBox(height: 20),
-                Container(
-                  width: 300,
-                  height: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => antrianMata()),
-                      );
-                    },
-                    child: Text('Antrian Poli Mata'),
+                SizedBox(height: 10),
+                Text(
+                  'Daftar Antrian',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
                 ),
-                SizedBox(height: 20,),
-                Container(
-                  width: 300,
-                  height: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
+                SizedBox(height: 30),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        width: 250,
+                        height: 100, // Perbesar tinggi tombol
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(16), // Perbesar padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => antrianUmum()),
+                            );
+                          },
+                          child: Text(
+                            'Antrian Poli Umum',
+                            style: TextStyle(fontSize: 24), // Perbesar ukuran teks
+                          ),
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => antrianGigi()),
-                      );
-                    },
-                    child: Text('Antrian Poli Gigi'),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: 300,
-                  height: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
+                      SizedBox(height: 25),
+                      Container(
+                        width: 250,
+                        height: 100, // Perbesar tinggi tombol
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(16), // Perbesar padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => antrianMata()),
+                            );
+                          },
+                          child: Text(
+                            'Antrian Poli Mata',
+                            style: TextStyle(fontSize: 24), // Perbesar ukuran teks
+                          ),
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => antrianKandungan()),
-                      );
-                    },
-                    child: Text('Antrian Poli Kandungan'),
+                      SizedBox(height: 25),
+                      Container(
+                        width: 250,
+                        height: 100, // Perbesar tinggi tombol
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(16), // Perbesar padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => antrianGigi()),
+                            );
+                          },
+                          child: Text(
+                            'Antrian Poli Gigi',
+                            style: TextStyle(fontSize: 24), // Perbesar ukuran teks
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      Container(
+                        width: 250,
+                        height: 100, // Perbesar tinggi tombol
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(16), // Perbesar padding
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10), // Ini membuat bentuk bulat
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => antrianKandungan()),
+                            );
+                          },
+                          child: Text(
+                            'Antrian Poli Kandungan',
+                            style: TextStyle(fontSize: 24), // Perbesar ukuran teks
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
--- lib/antrianKandungan.dart
+++ lib/antrianKandungan.dart
@@ -10,12 +10,10 @@ class antrianKandungan extends StatefulWidget {
 
 class _antrianKandunganState extends State<antrianKandungan> {
   int _selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Antrian Poli Kandungan'),
-      ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -23,12 +21,44 @@ class _antrianKandunganState extends State<antrianKandungan> {
             fit: BoxFit.cover,
           ),
         ),
-        child: ListView(
-          padding: EdgeInsets.symmetric(vertical: 20.0),
+        child: Column(
           children: [
-            QueueItem(status: QueueStatus.pending,queueNumber: 3),
-            QueueItem(status: QueueStatus.missed,queueNumber: 2),
-            QueueItem(status: QueueStatus.processing,queueNumber: 1),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () {
+                      Navigator.pop(context); // Kembali ke halaman sebelumnya
+                    },
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        'Antrian Poli Kandungan',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 48), // Menambahkan lebar untuk menjaga jarak
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                children: [
+                  QueueItem(status: QueueStatus.missed, queueNumber: 4, patientName: 'Dewi'),
+                  QueueItem(status: QueueStatus.pending, queueNumber: 5, patientName: 'Ayu'),
+                  QueueItem(status: QueueStatus.processing, queueNumber: 6, patientName: 'Indah'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -44,40 +74,40 @@ class _antrianKandunganState extends State<antrianKandungan> {
   }
 }
 
-
 enum QueueStatus { missed, completed, pending, processing }
 
 class QueueItem extends StatelessWidget {
   final QueueStatus status;
   final int queueNumber;
+  final String patientName;
 
-  const QueueItem({Key? key,required this.status, required this.queueNumber}) : super(key: key);
+  const QueueItem({
+    Key? key,
+    required this.status,
+    required this.queueNumber,
+    required this.patientName,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Color statusColor = Colors.grey;
-    IconData iconData;
     String statusText;
 
     switch (status) {
       case QueueStatus.missed:
         statusColor = Colors.red;
-        iconData = Icons.error;
         statusText = 'Terlewat';
         break;
       case QueueStatus.completed:
         statusColor = Colors.green;
-        iconData = Icons.check_circle;
         statusText = 'Selesai';
         break;
       case QueueStatus.pending:
         statusColor = Colors.orange;
-        iconData = Icons.watch_later;
         statusText = 'Menunggu';
         break;
       case QueueStatus.processing:
         statusColor = Colors.blue;
-        iconData = Icons.access_time;
         statusText = 'Diproses';
         break;
     }
@@ -87,45 +117,71 @@ class QueueItem extends StatelessWidget {
       child: Center(
         child: Container(
           height: 100,
-          width: 400,
+          width: double.infinity,
           decoration: BoxDecoration(
-            color: Color.fromARGB(255, 171, 255, 172), // Warna hijau untuk setiap item antrian
+            color: Color.fromRGBO(9, 82, 89, 1), // Warna hijau untuk setiap item antrian
             borderRadius: BorderRadius.circular(10.0),
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              // Kolom kiri untuk nomor antrian yang diperbesar
               Container(
-                height: 50,
-                width: 150,
+                height: 100,
+                width: 100,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    bottomLeft: Radius.circular(10.0),
+                  ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Nomor Antrian: $queueNumber',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+                child: Center(
+                  child: Text(
+                    '$queueNumber',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      'Status: $statusText',
-                      style: TextStyle(
-                        color: statusColor,
+                  ),
+                ),
+              ),
+              // Kolom kanan untuk status dan nama pasien
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Status: $statusText',
+                        style: TextStyle(
+                          color: statusColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Nama: $patientName',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               if (status == QueueStatus.pending)
-                ElevatedButton(
-                  onPressed: () {
-                    // Tambahkan logika untuk memproses antrian di sini
-                  },
-                  child: Text('Proses'),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Tambahkan logika untuk memproses antrian di sini
+                    },
+                    child: Text('Proses'),
+                  ),
                 ),
             ],
           ),
